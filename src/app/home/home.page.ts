ba88c0f36bca63c8a06460a550435fc9e33e5a07
Ritesh <ritesh.panditi98@gmail.com>
2021-11-11 01:43:22 -0500

    Added a way to display the NFTs on the webpage with name and token IDs

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -21,8 +21,24 @@ export class HomePage {
     const options = {method: 'GET'}; 
 // method to get data from api and dsiplay repson in jsoon format
   fetch('https://api.opensea.io/api/v1/assets?owner=0x05DaE8B6A2F0AD8F6bA8287fd16e37EBa26Ef1b4&order_direction=desc&offset=0&limit=20', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
+  .then(response => response.json())   
+  .then(response => response.assets)
+  .then(response => this.appendData(response))
+
   .catch(err => console.error(err));
   }
+  appendData(data) {
+    var mainContainer = document.getElementById("myData");
+    for (var i = 0; i < data.length; i++) {
+        var div = document.createElement("div");
+        div.innerHTML = 'NFT: ' + data[i].token_id + "     " + data[i].asset_contract.name;
+        mainContainer.appendChild(div);
+        var img = document.createElement("img");
+        img.setAttribute("src", data[i].image_url);
+        mainContainer.appendChild(img);
+    }
 }
+
+ 
+}
+// Ritesh's version
