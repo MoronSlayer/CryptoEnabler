ab281930d22aaf1a11dea3b87c92896500004aa0
Vivek Kothari <vivekkot1998@gmail.com>
2021-11-17 01:04:29 +0530

    Added ionic storage and nav controller to pass data between pages.Color to toolbars as tertiary. Added search event to call onSearch function. Capturing of wallet Id was done.  Wallet Id can now be dynamically entered.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,18 +1,29 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { Storage } from '@ionic/storage-angular';
+import { NavController } from '@ionic/angular';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  walletId: string;
 
   constructor(
-    private router: Router
+    private router: Router,
+    private storage: Storage,
+    private navCtrl: NavController
   ) {}
 
-  onClick(){
-  this.router.navigateByUrl('/display-assets');
+  async onSearch(ev) {
+    this.walletId = ev.target.value;
+    await this.storage.create();
+    //setting a key(myWalletId) - value(walletId) pair to store the wallet id.
+    await this.storage.set('myWalletId', this.walletId);
+    await this.navCtrl.navigateForward(['/display-assets']);
+     this.router.navigateByUrl('/display-assets');
   }
 }
