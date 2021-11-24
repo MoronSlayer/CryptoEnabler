d56173dd8453d5da15e8beba4fb599917501a891
Ritesh <ritesh.panditi98@gmail.com>
2021-11-24 13:23:30 -0500

    working on the UI design of the app, added a hard-coded collapsible list , code should be cleaned

--- src/app/display-assets/display-assets.page.ts
+++ src/app/display-assets/display-assets.page.ts
@@ -41,15 +41,54 @@ export class DisplayAssetsPage implements OnInit {
     });
   }
 
+  // appendData(data) {
+  //   const mainContainer = document.getElementById('myData');
+  //   for (let i = 0; i < data.length; i++) {
+  //       const div = document.createElement('div');
+  //       div.innerHTML = 'NFT: ' + data[i].token_id + '     ' + data[i].asset_contract.name;
+  //       mainContainer.appendChild(div);
+  //       const img = document.createElement('img');
+  //       img.setAttribute('src', data[i].image_url);
+  //       mainContainer.appendChild(img);
+  //   }
+  
+  // appendData(data) {
+  //   const mainContainer = document.getElementById('myData');
+  //   for (let i = 0; i < data.length; i++) {
+  //       const NFT_card = document.createElement('ion-card');
+  //       // div.innerHTML = 'NFT: ' + data[i].token_id + '     ' + data[i].asset_contract.name;
+  //       mainContainer.appendChild(NFT_card);
+  //       const img = document.createElement('img');
+  //       img.setAttribute('src', data[i].image_url);
+  //       NFT_card.appendChild(img);
+  //       const NFT_content = document.createElement('ion-card-content');
+  //       NFT_content.setAttribute('class','card-content card-content-ios')
+  //       NFT_content.innerHTML = 'NFT: ' + data[i].token_id + '     ' + data[i].asset_contract.name
+  //       NFT_card.appendChild(NFT_content);
+  //   }
+  // }
   appendData(data) {
+    console.log(data);
     const mainContainer = document.getElementById('myData');
     for (let i = 0; i < data.length; i++) {
-        const div = document.createElement('div');
-        div.innerHTML = 'NFT: ' + data[i].token_id + '     ' + data[i].asset_contract.name;
-        mainContainer.appendChild(div);
+        const NFT_item = document.createElement('ion-list');
+        const NFT_card = document.createElement('ion-item');
+        NFT_item.appendChild(NFT_card);
+        const thumbnail = document.createElement('ion-thumbnail');
+        NFT_card.appendChild(thumbnail);
+        // div.innerHTML = 'NFT: ' + data[i].token_id + '     ' + data[i].asset_contract.name;
+        mainContainer.appendChild(NFT_card);
         const img = document.createElement('img');
         img.setAttribute('src', data[i].image_url);
-        mainContainer.appendChild(img);
+        thumbnail.appendChild(img);
+        const NFT_content_header = document.createElement('h1');
+        // NFT_content.setAttribute('class','card-content card-content-ios')
+        NFT_content_header.innerHTML = data[i].asset_contract.name
+        const NFT_token_id =document.createElement('p');
+        NFT_token_id.innerHTML = 'NFT: ' + data[i].token_id 
+        NFT_card.appendChild(NFT_content_header);
+        NFT_card.appendChild(NFT_token_id);
     }
-}
+  }
+
 }
