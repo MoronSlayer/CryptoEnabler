aa4e723bb402dd835228361b656fd492217aeff0
Ritesh <ritesh.panditi98@gmail.com>
2021-12-04 15:04:37 -0500

    unnecessary comments removed

--- src/app/display-assets/display-assets.page.ts
+++ src/app/display-assets/display-assets.page.ts
@@ -41,19 +41,7 @@ export class DisplayAssetsPage implements OnInit {
     });
   }
 
-  // appendData(data) {
-  //   const mainContainer = document.getElementById('myData');
-  //   for (let i = 0; i < data.length; i++) {
-  //       const div = document.createElement('div');
-  //       div.innerHTML = 'NFT: ' + data[i].token_id + '     ' + data[i].asset_contract.name;
-  //       mainContainer.appendChild(div);
-  //       const img = document.createElement('img');
-  //       img.setAttribute('src', data[i].image_url);
-  //       mainContainer.appendChild(img);
-  //   }
-  // }  
   appendData(data) {
-    // console.log(data);
     const mainContainer = document.getElementById('myData');
     for (let i = 0; i < data.length; i++) {
         const NFT_item = document.createElement('ion-list');
@@ -62,25 +50,18 @@ export class DisplayAssetsPage implements OnInit {
         const thumbnail = document.createElement('ion-avatar');  
         thumbnail.setAttribute('slot','start');      
         NFT_card.appendChild(thumbnail);
-        // div.innerHTML = 'NFT: ' + data[i].token_id + '     ' + data[i].asset_contract.name;
         mainContainer.appendChild(NFT_card);
         const img = document.createElement('img');        
         img.setAttribute('src', data[i].image_url);
         thumbnail.appendChild(img);
         const NFT_content_header = document.createElement('p');
         NFT_content_header.setAttribute('class','info');
-        // NFT_content_header.setAttribute('slot','end');
         const NFT_end = document.createElement('p');
         NFT_end.setAttribute('class','info');
         NFT_end.setAttribute('slot','end');
         NFT_content_header.innerHTML = data[i].asset_contract.name + '\n' +'<br>' + 'NFT: ' + data[i].token_id 
-        // const NFT_token_id =document.createElement('p');
-        // NFT_token_id.setAttribute('slot','end');
-        // NFT_token_id.innerHTML = 'NFT: ' + data[i].token_id 
-        // NFT_card.appendChild(NFT_content_header);
         NFT_end.appendChild(NFT_content_header);
         NFT_card.appendChild(NFT_end);
-        // NFT_card.appendChild(NFT_token_id);
     }
 };
 
