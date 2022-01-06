e0426e19e647254ea9cd2668ec37b9b6fea7d3e8
Vivek Kothari <vivekkot1998@gmail.com>
2022-01-07 01:49:18 +0530

    Changed local storage from ionic to capacitor( displatAssetPage.ts and homePage.ts ). Added HttpClient of angular, replacing js fetch. Code refactoring. displayAsset page modified.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { Storage } from '@ionic/storage-angular';
-import { NavController } from '@ionic/angular';
+import { Storage } from '@capacitor/storage';
+
 
 @Component({
   selector: 'app-home',
@@ -14,16 +14,15 @@ export class HomePage {
 
   constructor(
     private router: Router,
-    private storage: Storage,
-    private navCtrl: NavController
-  ) {}
+  ) {
+  }
 
   async onSearch(ev) {
     this.walletId = ev.target.value;
-    await this.storage.create();
-    //setting a key(myWalletId) - value(walletId) pair to store the wallet id.
-    await this.storage.set('myWalletId', this.walletId);
-    await this.navCtrl.navigateForward(['/display-assets']);
+    await Storage.set({
+      key: 'myWalletId',
+      value: this.walletId
+    });
      this.router.navigateByUrl('/display-assets');
   }
 }
